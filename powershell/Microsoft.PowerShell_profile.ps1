89c050c2f0a1b8a24644f790fd8ea5191e5c86fc
vhqn <vhqn@example.com>
2026-05-13 16:24:46 +0800

    add ctrl d to exit powershell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 Set-Alias -Name trae -Value "D:\software\Trae CN\bin\trae-cn.cmd"
+Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
 function touch($Path) { New-Item -ItemType File -Path $Path }
 if (Test-Path Alias:rm) { Remove-Item Alias:rm -Force }
