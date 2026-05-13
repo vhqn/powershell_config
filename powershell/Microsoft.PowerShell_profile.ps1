b58dfd63c0b9f808e4dcae10f39d1c6500483cfa
vhqn <vhqn@example.com>
2026-05-13 16:19:46 +0800

    add ls

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,10 @@ Set-Alias -Name trae -Value "D:\software\Trae CN\bin\trae-cn.cmd"
 function touch($Path) { New-Item -ItemType File -Path $Path }
 if (Test-Path Alias:rm) { Remove-Item Alias:rm -Force }
 function rm($Path) { Remove-Item -Path $Path -Recurse -Force }
+if (Test-Path Alias:ls) { Remove-Item Alias:ls -Force }
+function ls { Get-ChildItem @args | Format-Wide -Property Name -AutoSize }
 function l { Get-ChildItem @args | Sort-Object LastWriteTime }
+function open($Path) { Invoke-Item -Path $Path }
 
 if (Test-Path Alias:cd) { Remove-Item Alias:cd -Force }
 function cd {
